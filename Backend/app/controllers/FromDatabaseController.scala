7f85163978713c94e161329d69d10ec88a7d3efd
sergaben <gabense@hotmail.com>
2017-12-11 00:29:06 +0000

    change config file and handle future callback

--- Backend/app/controllers/FromDatabaseController.scala
+++ Backend/app/controllers/FromDatabaseController.scala
@@ -5,10 +5,16 @@ import javax.inject.Inject
 import database.CourseRepositoryImpl
 import play.api.mvc.{AbstractController, ControllerComponents}
 
+import scala.util.{Failure, Success}
+
 class FromDatabaseController @Inject()(cc:ControllerComponents,courseRepositoryImpl: CourseRepositoryImpl) extends AbstractController(cc){
 
   def fromDatabase = Action{
-    println(courseRepositoryImpl.getAll())
+    val allCourses = courseRepositoryImpl.getAll()
+    allCourses onComplete{
+      case Success(courses) => for (course <- courses) println(course)
+      case Failure(t) => println("An error has occured: " + t.getMessage)
+    }
     Ok
   }
 
