28ade67ce0bd886bb60c3b496b0aca0fe85139d4
sergaben <gabense@hotmail.com>
2017-12-15 21:09:29 +0000

    fixing login controller

--- Backend/app/controllers/Login.scala
+++ Backend/app/controllers/Login.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject.Inject
 
 import database.KingstonStudentRepositoryImpl
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import scala.concurrent.ExecutionContext
@@ -10,16 +12,28 @@ import scala.concurrent.ExecutionContext
 class Login @Inject()(cc:ControllerComponents, kingstonStudentRepositoryImpl: KingstonStudentRepositoryImpl)
                      (implicit executionContext:ExecutionContext) extends AbstractController(cc){
 
-  def login = Action(parse.json){ req =>
+  case class LoginRequest(nickname:String,password:String)
+
+  implicit val LoginRequestReads: Reads[LoginRequest] = (
+    (JsPath \ "nickname").read[String] and
+      (JsPath \ "matchPassword").read[String]
+    )(LoginRequest.apply _)
+
+  implicit val LoginRequestWrites = new Writes[LoginRequest] {
+    def writes(loginRequest: LoginRequest) = Json.obj(
+      "nickname" -> loginRequest.nickname,
+      "matchPassword" -> loginRequest.password
+    )
+  }
+
+  def login = Action{ req =>
     // get the nickname and password from frontend
     // get the nickname and password from database
     // check that the password matched the one in the database
     // if it matches then send true otherwise send false
-    val nickname = (req.body \ "nickname").as[String].map{
-      name => Ok(name.toString)
-    }
-    val password = (req.body \ "matchPassword").as[String]
-    Ok
+    val json = req.body.asJson.get
+    val loginRequest = json.as[LoginRequest]
+    Ok(loginRequest.nickname)
   }
 
 }
