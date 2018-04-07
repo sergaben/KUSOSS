76aa98f9977a7baf1009ffa7e41a21a4612d084c
sergaben <gabense@hotmail.com>
2018-04-07 17:24:50 +0100

    testing

--- Backend/app/controllers/SignUp.scala
+++ Backend/app/controllers/SignUp.scala
@@ -7,7 +7,7 @@ import org.mindrot.jbcrypt.BCrypt
 import play.api.data.Forms._
 import play.api.data._
 import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
 
@@ -17,7 +17,7 @@ class SignUp @Inject()(cc:ControllerComponents, kingstonStudentRepositoryImpl: K
 
 
 
-  val userForm: Form[KingstonStudent] = Form (
+  private val userForm: Form[KingstonStudent] = Form (
     mapping(
       "id" -> optional(number),
       "nickname"->nonEmptyText,
@@ -32,26 +32,52 @@ class SignUp @Inject()(cc:ControllerComponents, kingstonStudentRepositoryImpl: K
 
 
 //TODO - check if user is registered already checking the nickname and email
-    def signup = Action{ req => // does this runs this query in blocking mode?
-      userForm.bindFromRequest.fold(
-        formWithErrors => {
-          // binding failure, retrieve the form with errors
-          BadRequest(Json.obj("status"->"OK","errors"->Json.toJson(formWithErrors)))
-        },
-        userData =>{
-          //binding success, you get the actual value
-          val hashedPassword = BCrypt.hashpw(userData.password,BCrypt.gensalt())
-          val kStudentWithHashedPassword = userData.copy(password = hashedPassword)
-          println(kStudentWithHashedPassword)
-          insertStudentIntoDatabase(kStudentWithHashedPassword)
-          Ok(Json.obj("status"->"OK","signup"->true))
-        }
-      )
+    def signup = Action{ implicit req => // does this runs this query in blocking mode?
+      signUpFormAsJson()
+//      userForm.bindFromRequest.fold(
+//        formWithError => {
+//          // binding failure, retrieve the form with errors
+//          val kstudent = formWithError.get.as[KingstonStudent]
+//          BadRequest(Json.obj("status"->"OK","errors"->kstudent))
+//        },
+//        userData =>{
+//          //binding success, you get the actual value
+//          val hashedPassword = BCrypt.hashpw(userData.password,BCrypt.gensalt())
+//          val kStudentWithHashedPassword = userData.copy(password = hashedPassword)
+//          println(kStudentWithHashedPassword)
+//          insertStudentIntoDatabase(kStudentWithHashedPassword)
+//          Ok(Json.obj("status"->"OK","signup"->true))
+//
+//        }
+//
+//      )
+//       val json = req.body.asJson.get
+//       val kStudent = json.as[KingstonStudent]
+//
+//       val kStudentWithHashedPassword = kingstonStudent.copy(password = hashedPassword)
+//       println(kStudentWithHashedPassword)
 
+//       Ok
     }
   // then run query insert into database
     private def insertStudentIntoDatabase(kingstonStudent: KingstonStudent): Unit ={
       kingstonStudentRepositoryImpl.insert(kingstonStudent)
+    }
+
+    private def signUpFormAsJson[A]()(implicit request: Request[A]):Result = {
+      def failure(badForm: Form[KingstonStudent]) = {
+        implicit val messages = messagesApi.preferred(request)
+        BadRequest(badForm.errorsAsJson)
+      }
+
+      def success(input: KingstonStudent) = {
+        val hashedPassword = BCrypt.hashpw(input.password,BCrypt.gensalt())
+        val kStudentWithHashedPassword = input.copy(password = hashedPassword)
+        println(kStudentWithHashedPassword)
+        insertStudentIntoDatabase(kStudentWithHashedPassword)
+        Ok(Json.obj("status"->"OK","signup"->true))
+      }
+      userForm.bindFromRequest().fold(failure,success)
     }
 
 
