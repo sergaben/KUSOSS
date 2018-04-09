fa75a952bb03f7961b154cb78373c11e14b56c70
sergaben <gabense@hotmail.com>
2018-04-09 16:27:30 +0100

    solving login backend issues

--- Backend/app/database/KingstonStudentRepositoryImpl.scala
+++ Backend/app/database/KingstonStudentRepositoryImpl.scala
@@ -30,8 +30,8 @@ class KingstonStudentRepositoryImpl @Inject()(protected val dbConfigProvider:Dat
   override def updateOrInsertToken(id:Option[Int],nickname: String,email:String,password:String,subject:String,typeOfStudy:String,loginToken:Option[String]): Future[Int] = {
     val tokenLoginAsOption:Option[String] = Option(UUID.randomUUID().toString)
     val getResult = for{
-      existing <- KStudents.filter(_.loginToken === loginToken).result.headOption
-      row      = existing.map(_.copy(loginToken=loginToken)) getOrElse KingstonStudent(id,nickname,email,password,subject,typeOfStudy,tokenLoginAsOption)
+      existing <- KStudents.filter(_.nickname === nickname).result.headOption
+      row      = existing.map(_.copy(loginToken=tokenLoginAsOption)) getOrElse KingstonStudent(id,nickname,email,password,subject,typeOfStudy,tokenLoginAsOption)
       result <- KStudents.insertOrUpdate(row)
     } yield result
     db.run(getResult)
