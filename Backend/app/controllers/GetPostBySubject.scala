9a328308215ce9b9aed499377e8cd6823c8575d6
sergaben <gabense@hotmail.com>
2018-04-17 19:28:30 +0100

    streaming solving issues

--- Backend/app/controllers/GetPostBySubject.scala
+++ Backend/app/controllers/GetPostBySubject.scala
@@ -34,10 +34,14 @@ class GetPostBySubject @Inject()(cc:ControllerComponents, postRepositoryImpl: Po
       "subject" -> getPostsRequest.subject
     )
   }
+  private def validateJson[A:Reads] = parse.json.validate(
+    _.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
+  )
 
-  def getPosts: Action[AnyContent] = Action.async{ implicit req=>
-    println(req.body.asJson.getOrElse("no subject"))
-    val postSource = Source.fromPublisher(postRepositoryImpl.getAllPostsBySubject("no subject"))
+
+  def getPosts: Action[GetPostsRequest] = Action.async(validateJson[GetPostsRequest]){ implicit req=>
+    println(req.body.subject)
+    val postSource = Source.fromPublisher(postRepositoryImpl.getAllPostsBySubject(req.body.subject))
     postSource.runForeach(post => println(post.toString))(materializer)
     Future.successful(Ok(Json.obj("status"->"OK","stream"->"OK")))
   }
