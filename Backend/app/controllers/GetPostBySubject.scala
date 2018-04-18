a88978b7afacaa03901cca6555065748d508a704
sergaben <gabense@hotmail.com>
2018-04-18 13:53:38 +0100

    SSE to front end

--- Backend/app/controllers/GetPostBySubject.scala
+++ Backend/app/controllers/GetPostBySubject.scala
@@ -9,7 +9,12 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import database.PostRepositoryImpl
 import javax.inject.Inject
+import models.Post
+import play.api.http.ContentTypes
+import play.api.libs.EventSource
+import play.api.libs.EventSource.EventDataExtractor
 import play.api.libs.json._
+import play.api.mvc.WebSocket.MessageFlowTransformer
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -22,9 +27,11 @@ import scala.concurrent.{ExecutionContext, Future}
   */
 class GetPostBySubject @Inject()(cc:ControllerComponents, postRepositoryImpl: PostRepositoryImpl)
                                 (implicit executionContext:ExecutionContext) extends AbstractController(cc) {
+  implicit val postFormat = Post.PostFormat
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
-
+  implicit val messageFlowTransformer = MessageFlowTransformer.jsonMessageFlowTransformer[Post,Post]
+  implicit val postEvents : EventDataExtractor[Post] = EventDataExtractor(postFormat.writes(_).toString())
   case class GetPostsRequest(subject:String)
 
   implicit val GetPostsRequestReads: Reads[GetPostsRequest] = (JsPath \ "subject").read[String].map(GetPostsRequest.apply _)
@@ -39,10 +46,11 @@ class GetPostBySubject @Inject()(cc:ControllerComponents, postRepositoryImpl: Po
   )
 
 
-  def getPosts: Action[GetPostsRequest] = Action.async(validateJson[GetPostsRequest]){ implicit req=>
+  def getPosts(subject:String): Action[GetPostsRequest] = Action.async(validateJson[GetPostsRequest]){ implicit req=>
     println(req.body.subject)
+    println(subject)
     val postSource = Source.fromPublisher(postRepositoryImpl.getAllPostsBySubject(req.body.subject))
     postSource.runForeach(post => println(Json.toJson(post)))(materializer)
-    Future.successful(Ok(Json.obj("status"->"OK","stream"->"OK")))
+    Future.successful(Ok.chunked(postSource via EventSource.flow[Post]).as(ContentTypes.EVENT_STREAM))
   }
 }
